ee6b1fa1480dd079b0564cddc7825554dc548bd8
Rafel Barbany <rafel.barbany@suez.com>
2021-05-12 17:09:13 +0200

    multiapps subnets

---
+++ infrastructure/terraform/SUEZSpain/SUEZSpain-PRO-Subscription/SUEZSpain-PRO-vnet/SUEZSpain-PROmultiapps-Subnet/variables.tf
@@ -0,0 +1,71 @@
+variable "tenant_id" {
+    type = string
+}
+
+variable "subscription_id" {
+    type = string
+}
+
+variable "prefix" {
+    type = string
+    default = "SUEZSpain"
+}
+
+variable "environment" {
+    type = string
+}
+
+variable "project" {
+    type = string
+    default = "drop"
+}
+
+variable "virtual_network_name" {
+    type = string
+}
+
+variable "vnet_resource_group_name" {
+    type = string
+}
+
+variable "subnetCIDR" {
+    type = string
+}
+
+
+
+
+
+variable "location" {}
+
+variable "tags" {
+    type = map
+}
+
+variable "region" {
+    type = string
+    default = "we"
+}
+
+variable "resourceFunction" {
+    type = string
+}
+
+variable "environment" {
+    type = string
+}
+
+variable "project" {
+    type = string
+    default = "drop"
+}
+
+
+
+variable "virtual_network_name" {
+    type = string
+}
+
+variable "vnet_resource_group_name" {
+    type = string
+}
